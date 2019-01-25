782ed6796c95e5010a3ebc22717545ead34c0c9d
Melika Kalbasi <melikalbasi@gmail.com>
2019-01-24 16:53:59 -0800

    import dependencies to app.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+import CharacterCard from "./components/CharacterCard";
+import characters from "./characters.json";
+
+

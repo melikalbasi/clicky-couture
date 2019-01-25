c08202560e959389b4d5168362991e83bde0177a
Melika Kalbasi <melikalbasi@gmail.com>
2019-01-24 16:55:13 -0800

    render page for app.js

--- src/App.js
+++ src/App.js
@@ -5,4 +5,45 @@ import Navbar from "./components/Navbar";
 import CharacterCard from "./components/CharacterCard";
 import characters from "./characters.json";
 
+class App extends Component {
+
+  state = {
+    characters,
+    currentScore: 0,
+    topScore: 0
+  }
+
+
+  componentDidMount() {
+    this.setState({characters: this.shuffle(this.state.characters)})
+  };
+
+
+  shuffle = data => {
+      for (let i = data.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [data[i], data[j]] = [data[j], data[i]];
+      }
+      return data;
+  };
+
+
+
+  render() {
+    return (
+     <div>
+       <Navbar currentScore={this.state.currentScore} topScore={this.state.topScore}/>
+       {this.state.characters.map(character => (
+         <CharacterCard 
+         key={character.id}
+         character={character}
+         click={this.handleClick}
+         />
+       ))}
+     </div>
+    );
+  }
+}
+
+export default App;
 
